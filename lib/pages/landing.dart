7649501862d10fdba74eaff825419db4bbe98b89
MarkyMark <mgelabert3@gmail.com>
2018-04-28 22:53:02 +0200

    second screen

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -4,17 +4,37 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return new Material(
-      color: Colors.greenAccent,
+      color: Colors.green[400],
       child: new InkWell(
+        onTap: () =>print("we tapped the page!"),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text("heyy"),
-            new Text("tap to start"),
+            new Text("WELCOMEZ", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+            new Text("tap to start", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),),
           ]
-        )
+        ) 
       )
     );
   }
 
+}
+
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return new Material(
+      color: Colors.red[400],
+      child: new InkWell(
+        onTap: () => print("we tapped the page!"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("BACK", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+          ],
+        )
+      )
+    )
+  }
 }
